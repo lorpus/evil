81a3e972e3985c8666e2b0d3a50283dbaba0ac23
ouwou <26526779+ouwou@users.noreply.github.com>
2019-06-05 14:00:45 -0400

    walkspeed in config

--- gamemode/config.lua
+++ gamemode/config.lua
@@ -4,8 +4,8 @@ AddCSLuaFile()
 local configuration = {
     Debug = true,    // set to true to enable debugging and lots of verbosity
 
-    Defaults = { // all of the following keys are required, do not remove them but you can change them
-    }
+    PlayerWalkspeed = 200,
+    PlayerRunspeed  = 320
 }
 
 function ApplyConfiguration(tab)
