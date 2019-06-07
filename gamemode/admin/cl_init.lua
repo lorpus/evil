b980f00577842327a8885b2af99ede5c5ec738f2
ouwou <26526779+ouwou@users.noreply.github.com>
2019-06-07 17:35:43 -0400

    camera views in waiting round

--- gamemode/admin/cl_init.lua
+++ gamemode/admin/cl_init.lua
@@ -1,1 +1,10 @@
-//
+concommand.Add("evil_getcamang", function(ply, cmd, args, argStr)
+    local pos = LocalPlayer():EyePos()
+    local ang = LocalPlayer():EyeAngles()
+    print(string.format([[
+{
+    pos = Vector(%f, %f, %f),
+    ang = Angle(%f, %f, %f)
+}
+    ]], pos.x, pos.y, pos.z, ang.p, ang.y, ang.r))
+end)
--- gamemode/gamesystem/cl_init.lua
+++ gamemode/gamesystem/cl_init.lua
@@ -1,1 +1,10 @@
-//
+hook.Add("CalcView", "cameraview", function(ply, vOrigin, qAngles, flFov, flZnear, flZfar)
+    if Round:GetRound() == ROUND_WAITING then
+        local iCamera = math.floor(CurTime() / 5 % #Map.cameras) + 1
+        local oCam = Map.cameras[iCamera]
+        return {
+            origin = oCam.pos,
+            angles = oCam.ang
+        }
+    end
+end)
