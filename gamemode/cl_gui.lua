59f7a719ad2e1c6fffe012ea99d7e8b24723810e
ouwou <26526779+ouwou@users.noreply.github.com>
2019-06-13 18:13:09 -0500

    made new gui box for la opener

--- gamemode/cl_gui.lua
+++ gamemode/cl_gui.lua
@@ -3,13 +3,33 @@ surface.CreateFont("ebilfont", {
     size = ScreenScale(40)
 })
 
+surface.CreateFont("beteterebilfont", {
+    font = "Verdana",
+    size = ScreenScale(20)
+})
+
 surface.CreateFont("ebilfontsmaller", {
     font = "Verdana",
     size = ScreenScale(20)
 })
 
+
 function guitest()
-    
+    local sw, sh = ScrW(), ScrH()
+    frame = vgui.Create("DFrame")
+    frame:SetSize(sw, sh)
+    frame:SetDraggable(false)
+    frame:SetTitle("")
+    frame:MakePopup()
+    function frame:Paint(w, h)
+        draw.RoundedBox(1, 0, 0, w, h, Color(0, 0, 0))
+
+        // shittitle
+        local txt = "EBILIEST GAME THERE EVER WAS DERP :333 oWo"
+        surface.SetFont("beteterebilfont")
+        local TxtW, TxtH = surface.GetTextSize("EBILIEST GAME THERE EVER WAS DERP :333 oWo")
+        draw.DrawText(txt, "beteterebilfont", w/2 - TxtW/2, 0, color_white)
+    end
 end
 
 hook.Add("HUDPaint", "OnloadGUIInit", function()
