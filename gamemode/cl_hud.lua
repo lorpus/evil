f4e9f4432296d8c833b328a35c9d065ff69351a1
ouwou <26526779+ouwou@users.noreply.github.com>
2019-06-17 00:22:26 -0500

    Added Health + Stam bar additions Made it any res compatible

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,56 +1,64 @@
 local OldHealth = 0
 local flLastRatio = 0
-local HealthMaterial = CreateMaterial("healthmuhterial", "UnlitGeneric", {
-    ["$basetexture"] = "detail/metal_detail_01"
+local oldAlpha = 0
+local HealthMaterial = CreateMaterial("blud", "UnlitGeneric", {
+    ["$basetexture"] = "ebil/blood",
+    ["$basetexturetransform"] = "scale 2 2"
 })
+
 
 hook.Add("HUDPaint", "Screen_Attributes", function()
     //if LocalPlayer():IsBoss() then return end
+
     local nWide, nTall = ScreenScale(110), ScreenScale(50) // n case people deside to change res midgame
     local nScrW, nScrH = ScrW(), ScrH()
+    
     local nRoundness = 0
     local PadX, PadY = 10, 10
+    local nSmallBoxT = nTall / 2
 
     local ColorA = Color(25, 25, 25)
-    local StamColorA = Color(255, 255, 255)
-    local StamColorB = Color(255, 255, 255, 25)
-    local HealthColorA = Color(255, 0, 0)
-    local HealthColorB = Color(255, 0, 0, 25)
+    local StamColorA = Color(0, 0, 255)
+    local StamColorB = {50, 50, 255, math.abs(math.sin(CurTime() * 0.7)) * (50 - 25) + 25}
+    local HealthColorA = {255, 50, 50, math.abs(math.sin(CurTime() * 5)) * (50 - 25) + 25}
+    local BarOutlineColor = {75, 0, 0}
 
-    local nExtra = 5
-    draw.RoundedBox(nRoundness, -nRoundness, (nScrH - nTall) + nRoundness + PadY + nExtra, nWide, (nTall - PadY) - nExtra, ColorA)
+    HealthColorA = Color(unpack(HealthColorA))
+    StamColorB = Color(unpack(StamColorB))
 
-    nTall = nTall - 50
-    draw.RoundedBox(nRoundness, nWide - (nRoundness * 2), (nScrH - nTall) + nRoundness, (nWide / 2), nTall, ColorA)
+    // Main Elements
+    draw.RoundedBox(nRoundness, -nRoundness, (nScrH - nTall) + nRoundness + PadY, nWide, (nTall - PadY), ColorA)
+    draw.RoundedBox(nRoundness, nWide - (nRoundness * 2), ((nScrH - nTall) + nSmallBoxT) + nRoundness, (nWide / 2), nTall - nSmallBoxT, ColorA)
 
     // Health Bar
-    local HealthBarW = nWide + (((nWide / 2) - nRoundness * 2) - PadX * 2)
+    local HealthBarW, HealthBarT = nWide + (((nWide / 2) - nRoundness * 2) - PadX * 2), (nTall - nSmallBoxT) - (PadY * 2)
     local MaxHealth = LocalPlayer():GetMaxHealth()
 
     local Health = LocalPlayer():Health()
     Health = Lerp(0.1, OldHealth, Health)
     OldHealth = Health
-
-    local NewHealthBarW, HealthBarT = (Health / MaxHealth) * HealthBarW, ScreenScale(12.5)
+    local NewHealthBarW = (Health / MaxHealth) * HealthBarW
     
-    draw.RoundedBox(0, PadX, (nScrH - HealthBarT / 2) - nTall / 2, HealthBarW, HealthBarT, HealthColorB)
-    surface.SetDrawColor(HealthColorA)
+    draw.RoundedBox(0, PadX, (nScrH - HealthBarT) - PadX, HealthBarW, HealthBarT, HealthColorA)
+    surface.SetDrawColor(Color(unpack(BarOutlineColor)))
     surface.SetMaterial(HealthMaterial)
-    surface.DrawTexturedRect(PadX, (nScrH - HealthBarT / 2) - nTall / 2, NewHealthBarW, HealthBarT) 
+    surface.DrawTexturedRectUV(PadX, (nScrH - HealthBarT) - PadX, NewHealthBarW, HealthBarT, 0, 0, NewHealthBarW / ScreenScale(32), HealthBarT / ScreenScale(32))
+    surface.DrawOutlinedRect(PadX, (nScrH - HealthBarT) - PadX, HealthBarW, HealthBarT, 0, 0, NewHealthBarW / ScreenScale(32), HealthBarT / ScreenScale(32)) 
 
     // Stamina Bar
 
     local flStamina = LocalPlayer():GetNWFloat("stamina")
     local flMaxStamina = Stamina.maxstamina
     local flRatio = flStamina / flMaxStamina
-    local StamBarW, StamBarT = (nWide - (PadX * 2)) - nRoundness, ScreenScale(12.5)
+    local StamBarW, StamBarT = (nWide - (PadX * 2)) - nRoundness, (nTall - nSmallBoxT) - (PadY * 2)
 
     flRatio = Lerp(0.1, flLastRatio, flRatio)
     flLastRatio = flRatio
 
-    nTall = nTall + 50
-    draw.RoundedBox(0, PadX, ((nScrH - StamBarT / 2) - nTall / 2) - PadY, StamBarW, StamBarT, StamColorB)
-    draw.RoundedBox(0, PadX, ((nScrH - StamBarT / 2) - nTall / 2) - PadY, flRatio * StamBarW, StamBarT, StamColorA)
+    draw.RoundedBox(0, PadX, ((nScrH - StamBarT) - nTall / 2), StamBarW, StamBarT, StamColorB)
+    draw.RoundedBox(0, PadX, ((nScrH - StamBarT) - nTall / 2), flRatio * StamBarW, StamBarT, StamColorA)
+    surface.DrawOutlinedRect(PadX, ((nScrH - StamBarT) - nTall / 2), StamBarW, StamBarT) 
+ 
 end)
 
 local hide = {
