624e1a5308c7ea0905d33017c42b7e05996afa14
ouwou <26526779+ouwou@users.noreply.github.com>
2019-06-19 18:35:24 -0500

    new stamina status hud added basic timer hud added

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,66 +1,80 @@
-local OldHealth = 0
-local flLastRatio = 0
-local oldAlpha = 0
-local HealthMaterial = CreateMaterial("blud", "UnlitGeneric", {
-    ["$basetexture"] = "ebil/blood",
-    ["$basetexturetransform"] = "scale 2 2"
+surface.CreateFont("evilfont1", {
+    font = "Verdana",
+    size = ScreenScale(10),
 })
 
+surface.CreateFont("evilfont2", {
+    font = "Verdana",
+    size = ScreenScale(7),
+})
+
+local function Timer()
+    local nScrW, nScrH = ScrW(), ScrH()
+    local TimerW, TimerT = ScreenScale(60), ScreenScale(15)
+    local ArbritratyText = nil
+    local FontA, FontB = "evilfont2", "evilfont1"
+    local ColorA = Color(25, 25, 25)
+    
+    draw.RoundedBox(0, nScrW / 2 - TimerW / 2, 0, TimerW, TimerT, ColorA)
+    
+    local nTimerValue = math.floor(Round:GetEndTime() - CurTime())
+    local TimerText = "Timer: " .. string.ToMinutesSeconds(math.floor(nTimerValue))
+
+    surface.SetFont(FontB)
+    local TextW, TextH = surface.GetTextSize(TimerText)
+    draw.DrawText(TimerText, FontB, nScrW / 2 - TextW / 2, TimerT / 2 - TextH / 2, Color(219, 255, 201))
+
+    if not ArbritratyText then return end
+
+    local Awide, Atall = TimerW, TimerT / 2
+
+    draw.RoundedBox(0, nScrW / 2 - Awide / 2, TimerT, Awide, Atall, ColorA)
+    
+    surface.SetFont(FontA)
+    local TextW, TextH = surface.GetTextSize(ArbritratyText)
+    draw.DrawText(ArbritratyText, FontA, nScrW / 2 - TextW / 2, TimerT + (Atall / 2 - TextH / 2), Color(255,255,255,255))
+
+end
+
+local Old = 0
+local StamMaterial = Material("ebil/stamina.png")
+
+local function PlayerStats()
+
+    if LocalPlayer():IsBoss() then return end
+
+    local nScrW, nScrH = ScrW(), ScrH()
+    local PadX, PadY = 10, 10
+    local Scaled = ScreenScale(48)
+    local x, y = 0, nScrH - Scaled
+
+    local ColorA = {25, 25, 25, 120}
+    local ColorB = {0, 0, 0}
+
+    local fmul = 1 - LocalPlayer():GetNWFloat("stamina") / Stamina.maxstamina
+
+    surface.SetDrawColor(unpack(ColorA))
+    surface.DrawRect(x, y - (PadY * 2), Scaled + (PadX * 2), Scaled + (PadY * 2))
+
+    surface.SetDrawColor(unpack(ColorB))
+    surface.DrawOutlinedRect(x, y - (PadY * 2), Scaled + (PadX * 2), Scaled + (PadY * 2))
+    
+    surface.SetDrawColor(255, 255, 255)
+    surface.SetMaterial(StamMaterial)
+    surface.DrawTexturedRectUV(x + PadX, nScrH - PadY - Scaled * (1 - fmul), Scaled, Scaled * (1 - fmul), 0, fmul, 1, 1)
+
+end
 
 hook.Add("HUDPaint", "Screen_Attributes", function()
-    //if LocalPlayer():IsBoss() then return end
-
-    local nWide, nTall = ScreenScale(110), ScreenScale(50) // n case people deside to change res midgame
-    local nScrW, nScrH = ScrW(), ScrH()
-    
-    local nRoundness = 0
-    local PadX, PadY = 10, 10
-    local nSmallBoxT = nTall / 2
-
-    local ColorA = Color(25, 25, 25)
-    local StamColorA = Color(0, 0, 255)
-    local StamColorB = Color(50, 50, 255, math.abs(math.sin(CurTime() * 0.7)) * (50 - 25) + 25)
-    local HealthColorA = Color(255, 50, 50, math.abs(math.sin(CurTime() * 5)) * (50 - 25) + 25)
-    local BarOutlineColor = Color(75, 0, 0)
-
-    // Main Elements
-    draw.RoundedBox(nRoundness, -nRoundness, (nScrH - nTall) + nRoundness + PadY, nWide, (nTall - PadY), ColorA)
-    draw.RoundedBox(nRoundness, nWide - (nRoundness * 2), ((nScrH - nTall) + nSmallBoxT) + nRoundness, (nWide / 2), nTall - nSmallBoxT, ColorA)
-
-    // Health Bar
-    local HealthBarW, HealthBarT = nWide + (((nWide / 2) - nRoundness * 2) - PadX * 2), (nTall - nSmallBoxT) - (PadY * 2)
-    local MaxHealth = LocalPlayer():GetMaxHealth()
-
-    local Health = LocalPlayer():Health()
-    Health = Lerp(0.1, OldHealth, Health)
-    OldHealth = Health
-    local NewHealthBarW = (Health / MaxHealth) * HealthBarW
-    
-    draw.RoundedBox(0, PadX, (nScrH - HealthBarT) - PadX, HealthBarW, HealthBarT, HealthColorA)
-    surface.SetDrawColor(BarOutlineColor)
-    surface.SetMaterial(HealthMaterial)
-    surface.DrawTexturedRectUV(PadX, (nScrH - HealthBarT) - PadX, NewHealthBarW, HealthBarT, 0, 0, NewHealthBarW / ScreenScale(32), HealthBarT / ScreenScale(32))
-    surface.DrawOutlinedRect(PadX, (nScrH - HealthBarT) - PadX, HealthBarW, HealthBarT, 0, 0, NewHealthBarW / ScreenScale(32), HealthBarT / ScreenScale(32)) 
-
-    // Stamina Bar
-
-    local flStamina = LocalPlayer():GetNWFloat("stamina")
-    local flMaxStamina = Stamina.maxstamina
-    local flRatio = flStamina / flMaxStamina
-    local StamBarW, StamBarT = (nWide - (PadX * 2)) - nRoundness, (nTall - nSmallBoxT) - (PadY * 2)
-
-    flRatio = Lerp(0.1, flLastRatio, flRatio)
-    flLastRatio = flRatio
-
-    draw.RoundedBox(0, PadX, ((nScrH - StamBarT) - nTall / 2), StamBarW, StamBarT, StamColorB)
-    draw.RoundedBox(0, PadX, ((nScrH - StamBarT) - nTall / 2), flRatio * StamBarW, StamBarT, StamColorA)
-    surface.DrawOutlinedRect(PadX, ((nScrH - StamBarT) - nTall / 2), StamBarW, StamBarT) 
+    PlayerStats()
+    Timer()
 end)
 
 local hide = {
     ["CHudHealth"] = true,
     ["CHudBattery"] = true,
-    ["CHudAmmo"] = true
+    ["CHudAmmo"] = true,
+    ["CHudSecondaryAmmo"] = true
 }
 
 hook.Add("HUDShouldDraw", "HideDasDefaults", function(n)
