db208c686495d165351350c55ad5956e27e2763c
ouwou <26526779+ouwou@users.noreply.github.com>
2019-06-25 15:30:41 -0400

    move color agaaain

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,4 +1,1 @@
 include("shared.lua")
-
-local color_black = Color(0, 0, 0)
-local color_red   = Color(125, 0, 0)
--- gamemode/network/cl_init.lua
+++ gamemode/network/cl_init.lua
@@ -1,3 +1,6 @@
+local color_black = Color(0, 0, 0)
+local color_red   = Color(125, 0, 0)
+
 function Evil:AddTextChat(str)
     chat.AddText(color_black, "[", color_red, "Evil", color_black, "]", color_white, ": ", str)
 end
