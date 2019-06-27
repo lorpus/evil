fad0ce269c491d2fc0543221a52c6f2de22d9239
ouwou <26526779+ouwou@users.noreply.github.com>
2019-06-26 21:33:57 -0500

    added special rounds

--- gamemode/specialrounds/cl_init.lua
+++ gamemode/specialrounds/cl_init.lua
@@ -1,21 +1,4 @@
-surface.CreateFont("Arial30", {
-    font = "Arial",
-    size = ScreenScale(10)
-})
 
-surface.CreateFont("Arial14", {
-    font = "Arial",
-    size = ScreenScale(6)
-})
 
-hook.Add("HUDPaint", "SpectatorHUD", function()
-    if LocalPlayer():IsSpectating() then
-        local text = Lang:Get("#Spec_Spectating")
-        if IsValid(LocalPlayer():GetObserverTarget()) then
-            text = LocalPlayer():GetObserverTarget():Nick()
-        end
 
-        draw.SimpleText(text, "Arial30", ScrW() / 2, ScrH() - 50, color_white, TEXT_ALIGN_CENTER)
-        draw.SimpleText(Lang:Format("#Spec_HUDInfo", { key = input.LookupBinding("+reload"):upper() }), "Arial14", ScrW() / 2, ScrH() - 20, color_white, TEXT_ALIGN_CENTER)
-    end
-end)
+concommand.Add("special", open)
