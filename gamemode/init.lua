be245a0d2f575f9e48ae205f40b2f3c3c30337b5
ouwou <26526779+ouwou@users.noreply.github.com>
2019-07-02 20:47:38 -0400

    Evil:Lock no longer fails when loading map fails

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,10 +1,11 @@
 AddCSLuaFile("cl_init.lua")
-include("shared.lua")
 
 function Evil:Lock(reason)
     Evil.bLocked = true
     Evil.strLockReason = reason or Lang:Get("#NoReasonGiven")
 end
+
+include("shared.lua")
 
 hook.Add("Think", "KickJoinedPlayers", function() // may as well do it here cuz if someone is mid-join they wont get kicked (i dont think)
     if not Evil.bLocked then return end
