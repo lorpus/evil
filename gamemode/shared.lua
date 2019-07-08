c6923d57e15dd3228250c4a16dd40eeb2cd0c5b8
ouwou <26526779+ouwou@users.noreply.github.com>
2019-07-07 22:49:47 -0400

    fix Evil:Lock on gamemode init

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -54,7 +54,7 @@ end
 
 if SERVER then
     function Evil:Lock(reason)
-        if Evil.TestingCVar:GetBool() then return end
+        if Evil.TestingCvar and Evil.TestingCVar:GetBool() then return end
         Evil.bLocked = true
         Evil.strLockReason = reason or Lang:Get("#NoReasonGiven")
     end
