275490199541a22be3c042fb56d1f6500eda87c3
ouwou <26526779+ouwou@users.noreply.github.com>
2019-07-08 17:20:06 -0500

    added slender_underground (no pos)

---
+++ gamemode/maps/slender_underground_r1_evpack.lua
@@ -0,0 +1,19 @@
+Map = {
+    cameras = {
+        
+    },
+
+    spawns = {
+        humans = {
+            
+        },
+
+        boss = {
+                       
+        }
+    },
+
+    pages = {
+
+    }
+}
