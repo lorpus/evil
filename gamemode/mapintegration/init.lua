05ed3a17a422417d6f66e05fe382ee4b846c918c
ouwou <26526779+ouwou@users.noreply.github.com>
2019-08-11 16:20:43 -0400

    allow maps to set page locations add map opt to prevent maps from setting them too

--- gamemode/mapintegration/init.lua
+++ gamemode/mapintegration/init.lua
@@ -14,3 +14,61 @@ hook.Add("RoundSet", "EvilMapResetVars", function(round)
         collected = 0
     end
 end)
+
+hook.Add("InitPostEntity", "EvilMapLoadOverrides", function()
+    if not istable(Map) then
+        return dbg.print("not loading map overrides because map table missing!")
+    end
+
+    if Map.ignore_overrides then
+        return dbg.print("not loading map overrides because map config explicitly disables them")
+    end
+
+    if istable(Map[1]) then
+        return dbg.print("not loading map overrides because map is mulit-config!")
+    end
+
+    local pageoverride = ents.FindByName("OverridePage")
+    if #pageoverride > 0 then
+        dbg.print("page override found")
+        table.Empty(Map.pages)
+    else
+        return
+    end
+
+    local singlepages = {}
+    local pagegroups  = {}
+    local pages = ents.FindByName("Page_*")
+    for _, page in pairs(pages) do
+        local name = page:GetName()
+        if name:lower() == "page" then
+            table.insert(singlepages, page)
+        else
+            local group = name:lower():match("_(%w+)")
+            if not pagegroups[group] then pagegroups[group] = {} end
+            table.insert(pagegroups[group], page)
+        end
+    end
+
+    dbg.print("loaded " .. tostring(#singlepages) .. " single page locations")
+    dbg.print("loaded " .. tostring(table.Count(pagegroups))  .. " page group locations")
+
+    for _, page in pairs(singlepages) do
+        table.insert(Map.pages, {
+            pos = page:GetPos(),
+            ang = page:GetAngles()
+        })
+    end
+
+    for _, group in pairs(pagegroups) do
+        local ins = {}
+        for _, page in pairs(group) do
+            table.insert(ins, {
+                pos = page:GetPos(),
+                ang = page:GetAngles()
+            })
+        end
+
+        table.insert(Map.pages, ins)
+    end
+end)
