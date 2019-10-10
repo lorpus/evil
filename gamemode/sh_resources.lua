01895743c82ddcc12274255211155109457501f9
ouwou <26526779+ouwou@users.noreply.github.com>
2019-10-10 16:29:17 -0400

    cleanup

--- gamemode/sh_resources.lua
+++ gamemode/sh_resources.lua
@@ -1,26 +1,13 @@
 if SERVER then
-    // resource.AddWorkshop("1780893440") // ecp 1
-    resource.AddWorkshop("1885257980") // ebc
-
-    /*if not EVIL_CONTENT_1_INSTALLED then
-        print("\n\n")
-        for i = 1, 10 do
-            print(Lang:Format("#ServerNoContent", { num = "1" }))
-        end
-    end*/
-
+    resource.AddWorkshop("1885257980") // base content
+    
     if not EVIL_BASE_CONTENT_INSTALLED then
         print("\n\n")
         for i = 1, 10 do
             Evil.Log(Lang:Format("#ServerNoContent", { num = "Base" }))
         end
     end
-else // cli
-    // placeholder
-    /*if not EVIL_CONTENT_1_INSTALLED then
-        Evil:AddTextChat(Lang:Format("#ClientNoContent", { num = "1" }))
-    end*/
-
+else
     if not EVIL_BASE_CONTENT_INSTALLED then
         Evil:AddTextChat(Lang:Format("#ClientNoContent", { num = "Base" }))
     end
