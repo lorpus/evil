445b14dda62a9d11b31e50d67f8f6e2236dc05b5
ouwou <26526779+ouwou@users.noreply.github.com>
2019-10-22 22:22:28 -0400

    localize frame

--- gamemode/gui/cl_gui.lua
+++ gamemode/gui/cl_gui.lua
@@ -20,6 +20,8 @@ local sHelp = Evil.Cfg.MainMenu.HelpText
 local nFadeSpeed = 6
 local nAvatarFadeSpeed = 2
 
+local frame
+
 local function FirstTimeGUI()
     local sw, sh = ScrW(), ScrH()
     frame = vgui.Create("DFrame")
